e162919d88bf840849808e85e5e5bf2a7fdbfb4e
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-03-09 01:00:17 +0300

    updated formatDate function

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,16 +1,18 @@
 const formatDate = (date) => {
-    let d = date;
-    let month = d.getMonth() + 1;
-    if (month < 10) {
-      month = ["0", month].join("");
-    }
-    let day = d.getDate();
-    if (day < 10) {
-      day = ["0", day].join("");
-    }
-    const year = d.getFullYear();
-    d = [day, month, year].join("-");
-    return d;
-  };
-  
-  export default formatDate;
+  let d = new Date(date);
+  let month = `${d.getMonth() + 1}`;
+  let day = `${d.getDate()}`;
+  const year = d.getFullYear();
+
+  if (month.length < 2) {
+    month = `0${month}`;
+  }
+
+  if (day.length < 2) {
+    day = `0${day}`;
+  }
+  d = [day, month, year].join("-");
+  return d;
+};
+
+export default formatDate;
